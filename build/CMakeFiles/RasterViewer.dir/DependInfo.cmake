f59635bd5c41588a5228173e6688ce15c4bd533c
Minerva <minervamcheng@gmail.com>
2020-12-14 23:57:40 -0500

    initial commit; starter code

---
+++ build/CMakeFiles/RasterViewer.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sensui/Documents/NYU/CG/cg-master/Assignment_6/src/RasterViewer.cpp" "/Users/sensui/Documents/NYU/CG/cg-master/Assignment_6/build/CMakeFiles/RasterViewer.dir/src/RasterViewer.cpp.o"
+  "/Users/sensui/Documents/NYU/CG/cg-master/Assignment_6/src/raster.cpp" "/Users/sensui/Documents/NYU/CG/cg-master/Assignment_6/build/CMakeFiles/RasterViewer.dir/src/raster.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DATA_DIR=\"/Users/sensui/Documents/NYU/CG/cg-master/Assignment_6/data/\""
+  "SDL_MAIN_HANDLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/sensui/Documents/NYU/CG/cg-master/ext/SDL/include"
+  "../../ext/eigen"
+  "../../ext/stb"
+  "../../ext/gif-h"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/sensui/Documents/NYU/CG/cg-master/Assignment_6/build/CMakeFiles/Assignment_6.dir/DependInfo.cmake"
+  "/Users/sensui/Documents/NYU/CG/cg-master/Assignment_6/build/SDL-build/CMakeFiles/SDL2-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
